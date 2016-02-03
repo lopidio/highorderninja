a325e093747fffad6431e09188d60ddb6429e3f3
Guigas <guilherme.moraes@outlook.com>
2016-02-02 22:49:29 -0200

    Creating colliders utils

--- implementation/src/main/java/br/com/guigasgame/collision/CollidersFilters.java
+++ implementation/src/main/java/br/com/guigasgame/collision/CollidersFilters.java
@@ -3,6 +3,32 @@ package br.com.guigasgame.collision;
 
 public class CollidersFilters
 {
+	public enum CategoryMask
+	{
+		PLAYER_ONE(0x0001),
+		PLAYER_TWO(0x0002),
+		PLAYER_THREE(0x0004),
+		PLAYER_FOUR(0x0008),
+		
+		BULLET(0x0020),
+		SCENERY(0x0040),
+		SINGLE_BLOCK(0x0080);
+
+		
+		int maskValue;
+
+		private CategoryMask(int maskValue)
+		{
+			this.maskValue = maskValue;
+		}
+
+		
+		public static int getAllPlayersMask()
+		{
+			return PLAYER_ONE.maskValue | PLAYER_TWO.maskValue | PLAYER_THREE.maskValue | PLAYER_FOUR.maskValue;
+		}
+		
+	}
 	///WHO I AM
 	public final static short CATEGORY_PLAYER_ONE 	= 0x0001;
 	public final static short CATEGORY_PLAYER_TWO 	= 0x0002;
@@ -10,7 +36,6 @@ public class CollidersFilters
 	public final static short CATEGORY_PLAYER_FOUR 	= 0x0008;
 	public final static short CATEGORY_PLAYER_MASK 	= CATEGORY_PLAYER_ONE | CATEGORY_PLAYER_TWO | CATEGORY_PLAYER_THREE | CATEGORY_PLAYER_FOUR;
 
-	
 	public final static short CATEGORY_BULLET 		= 0x0020;
 	public final static short CATEGORY_SCENERY 		= 0x0040;
 	public final static short CATEGORY_SINGLE_BLOCK = 0x0080;
@@ -18,5 +43,31 @@ public class CollidersFilters
 	///WHAT I COLLIDE WITH
 	public final static short MASK_PLAYER = CATEGORY_SCENERY | CATEGORY_PLAYER_MASK | CATEGORY_BULLET; // or ~CATEGORY_PLAYER
 	public final static short MASK_BULLET = CATEGORY_PLAYER_MASK | CATEGORY_SCENERY | CATEGORY_SINGLE_BLOCK; // or ~CATEGORY_MONSTER
-	public final static short MASK_SCENERY = -1; //also equals to 0xFFFF	
+	public final static short MASK_SCENERY = -1; //also equals to 0xFFFF
+	
+	private int maskValue;
+	
+	public static int getPlayerCategory(int playerID)
+	{
+		return 1 << (playerID - 1);
+	}
+	
+	
+	
+	public CollidersFilters addMask(CollidersFilters mask)
+	{
+		maskValue |= mask.maskValue;
+		return this;
+	}
+	
+	public CollidersFilters removeMask(CollidersFilters mask)
+	{
+		maskValue &= ~mask.maskValue;
+		return this;
+	}
+	
+	public int getMaskValue()
+	{
+		return maskValue;
+	}
 }
--- implementation/src/main/java/br/com/guigasgame/gameobject/hero/executor/HeroShooterExecutor.java
+++ implementation/src/main/java/br/com/guigasgame/gameobject/hero/executor/HeroShooterExecutor.java
@@ -1,12 +1,8 @@
 package br.com.guigasgame.gameobject.hero.executor;
 
-import org.jbox2d.common.Vec2;
-
 import br.com.guigasgame.gameobject.hero.GameHero;
 import br.com.guigasgame.gameobject.hero.state.HeroStatesProperties;
 import br.com.guigasgame.gameobject.projectile.Projectile;
-import br.com.guigasgame.gameobject.projectile.ProjectileIndex;
-import br.com.guigasgame.gameobject.projectile.Shuriken;
 
 
 public class HeroShooterExecutor
@@ -21,10 +17,10 @@ public class HeroShooterExecutor
 		this.gameHero = gameHero;
 	}
 
-	public void shoot(Shuriken shuriken)
+	public void shoot(Projectile projectile)
 	{
 		if (heroStatesProperties.canShoot)
-			gameHero.shoot(shuriken);
+			gameHero.shoot(projectile);
 	}
 
 }
